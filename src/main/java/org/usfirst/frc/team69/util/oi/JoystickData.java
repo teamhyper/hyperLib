36f286a16d3ee19cc09e22d75ce96c8dae9c76c6
James Hagborg <jameshagborg@gmail.com>
2017-01-31 12:32:07 -0500

    Document JoystickData

--- src/main/java/org/usfirst/frc/team69/util/oi/JoystickData.java
+++ src/main/java/org/usfirst/frc/team69/util/oi/JoystickData.java
@@ -24,11 +24,12 @@ public class JoystickData {
     /**
      * Construct a new JoystickData object with the given parameters
      * 
-     * @param port
-     * @param role
-     * @param type
-     * @param mapClass
-     * @param buttons
+     * @param port the port the joystick is plugged into
+     * @param role the role the joystick fills
+     * @param type the physical type of joystick
+     * @param mapClass the class inside the oi map representing the joystick
+     * @param buttons a list of buttons which are part of the joystick.  This object makes
+     * an immutable reference to the list passed in.
      */
     public JoystickData(int port, Role role, Type type, Class<?> mapClass, List<ButtonData> buttons) {
         m_port = port;
@@ -38,10 +39,43 @@ public class JoystickData {
         m_buttons = Collections.unmodifiableList(buttons);
     }
     
+    /**
+     * Get the port the joystick is plugged into
+     * @return the port the joystick is plugged into
+     */
     public int port() { return m_port; }
+    
+    /**
+     * Get the role of the joystick
+     * @return the role of the joystick
+     */
     public Role role() { return m_role; }
+    
+    /**
+     * Get the physical type of the joystick
+     * @return the type of the joystick
+     */
     public Type type() { return m_type; }
+    
+    /**
+     * Get a string representation of the name of the joystick.  This is the
+     * simple name of the class representing the joystick
+     * @return The name of the joystick
+     */
     public String name() { return m_class.getSimpleName(); }
+    
+    /**
+     * Get the class used to declare the joystick.  This should be a public
+     * static class in the oi map which had a {@link MapJoystick} annotation
+     * @return The class of the joystick
+     */
     public Class<?> mapClass() { return m_class; }
+    
+    /**
+     * Get an immutable list of buttons on the joystick.  This is actually a
+     * list of button/command mappings, so a single button number may be counted
+     * twice if there are multiple commands on it.
+     * @return A list of button/command mappings
+     */
     public List<ButtonData> buttons() { return m_buttons; }
 }
