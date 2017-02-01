8fb149e4eb5e4bac751bfd96257a2fcccef4a68d
James Hagborg <jameshagborg@gmail.com>
2017-01-31 23:27:50 -0500

    more debugging

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -9,13 +9,13 @@ echo "debug: cwd is $(pwd)"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages "https://${GH_TOKEN}@github.com/teamhyper/hyperLib" gh-pages &> /dev/null
+ls -la
 cd gh-pages
 echo "debug: cwd is now $(pwd)"
 git status
 
 echo "Copying docs over..."
 # Copy docs over
-
 mkdir -p doc/${DEPLOY_DIR}
 cp -Rf ${TRAVIS_BUILD_DIR}/build/docs/javadoc/* doc/${DEPLOY_DIR}
 
