d48698e0f9c2a6302586faf11a22f34312136463
James Hagborg <jameshagborg@gmail.com>
2017-02-01 00:27:40 -0500

    remove debugging stuff

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -7,28 +7,20 @@ DEPLOY_DIR=$1
 
 echo "Deploying docs to ${DEPLOY_DIR}"
 
-echo "Pulling existing github pages..."
-# Set up git
-
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 
+echo "Pulling from gh-pages"
 mkdir $HOME/gh-pages
 cd $HOME/gh-pages
-echo "debug: cwd is $(pwd)"
 git init
 git pull "https://github.com/teamhyper/hyperLib.git" gh-pages
-ls -la
-git status
-git branch
 
 echo "Copying docs over..."
-# Copy docs over
 mkdir -p doc/${DEPLOY_DIR}
 cp -Rf ${TRAVIS_BUILD_DIR}/build/docs/javadoc/* doc/${DEPLOY_DIR}
 
 echo "Committing and pushing..."
-# Commit and push changes
 git add -f .
 git commit -m "Add latest javadoc from build $TRAVIS_BUILD_NUMBER to $DEPLOY_DIR"
 git push -f "https://${GH_TOKEN}@github.com/teamhyper/hyperLib.git" +master:gh-pages &> /dev/null
