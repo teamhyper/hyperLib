c2814fd6e42876fa0d2c89c990a278f107833767
James Hagborg <jameshagborg@gmail.com>
2017-01-31 23:03:01 -0500

    Set up automatic deploy of docs

---
+++ scripts/deploy_docs.sh
@@ -0,0 +1,24 @@
+DEPLOY_DIR=$1
+
+echo "Deploying docs to ${DEPLOY_DIR}"
+
+echo "Pulling existing github pages..."
+# Set up git
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages "https://${GH_TOKEN}@github.com/teamhyper/hyperLib" gh-pages &> /dev/null
+
+echo "Copying docs over..."
+# Copy docs over
+cd gh-pages
+mkdir -p doc/${DEPLOY_DIR}
+cp -Rf ${TRAVIS_BUILD_DIR}/build/docs/javadoc/* doc/${DEPLOY_DIR}
+
+echo "Committing and pushing..."
+# Commit and push changes
+git add -f .
+git commit -m "Add latest javadoc from build $TRAVIS_BUILD_NUMBER to $DEPLOY_DIR"
+git push -fq origin gh-pages &> /dev/null
+
+echo "Docs successfully deployed"
