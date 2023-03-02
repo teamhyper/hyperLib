7be572895c4bee5ab1f88f040f0233cc63fa986c
Chris McGroarty <iammarix@gmail.com>
2023-03-02 16:54:25 -0500

    use speed for moving with limits

--- src/main/java/org/hyperonline/hyperlib/subsystem/HasForwardLimit.java
+++ src/main/java/org/hyperonline/hyperlib/subsystem/HasForwardLimit.java
@@ -5,8 +5,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import java.util.function.DoubleSupplier;
 
 public interface HasForwardLimit {
-    boolean canMoveForward();
     boolean isAtForwardLimit();
+    boolean canMoveForward(DoubleSupplier speed);
     boolean canMove(DoubleSupplier speed);
     Command autoResetSensorAtForwardLimit();
 }
