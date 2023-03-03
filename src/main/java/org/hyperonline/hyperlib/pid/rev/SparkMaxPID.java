334818b301f41293f4e6e155b411154218974cc4
Chris McGroarty <iammarix@gmail.com>
2023-03-02 22:47:55 -0500

    fix: SparkMaxPID cannot have a fNatural of 0 (used as a divisor)

--- src/main/java/org/hyperonline/hyperlib/pid/rev/SparkMaxPID.java
+++ src/main/java/org/hyperonline/hyperlib/pid/rev/SparkMaxPID.java
@@ -11,178 +11,185 @@ import org.hyperonline.hyperlib.pref.IntPreference;
 
 public class SparkMaxPID extends PrefPIDController {
 
-  private HYPER_CANSparkMax m_motor;
-  private IntPreference m_IZone;
-  private DoublePreference m_maxVelocityUnits, m_smartMaxVel, m_smartMinVel, m_smartMaxAccel;
-  private double m_setPoint;
-  private CANSparkMax.ControlType m_controlType;
-  private HYPER_CANSensorSendable m_sensor;
-  private SparkMaxPIDController m_pidController;
-  private int m_pidSlot;
+    private HYPER_CANSparkMax m_motor;
+    private IntPreference m_IZone;
+    private DoublePreference m_maxVelocityUnits, m_smartMaxVel, m_smartMinVel, m_smartMaxAccel;
+    private double m_setPoint;
+    private CANSparkMax.ControlType m_controlType;
+    private HYPER_CANSensorSendable m_sensor;
+    private SparkMaxPIDController m_pidController;
+    private int m_pidSlot;
 
-  private boolean useSmartMotion = false;
+    private boolean useSmartMotion = false;
 
-  public SparkMaxPID(
-      String name,
-      HYPER_CANSparkMax motor,
-      CANSparkMax.ControlType controlType,
-      double Kp,
-      double Ki,
-      double Kd,
-      int kIZone,
-      double tolerance,
-      double fNatural,
-      HYPER_CANSensorSendable sensorToUse,
-      int slotID) {
-    super(name, Kp, Ki, Kd, tolerance);
-    m_pidSlot = slotID;
-    m_motor = motor;
-    m_sensor = sensorToUse;
-    m_pidController = m_motor.getPIDController();
-    m_pidController.setFeedbackDevice(m_sensor.getSensor());
-    m_controlType = controlType;
-    m_IZone = m_prefs.addInt("IZone", kIZone);
-    m_maxVelocityUnits = m_prefs.addDouble("F Natural", fNatural);
+    public SparkMaxPID(
+            String name,
+            HYPER_CANSparkMax motor,
+            CANSparkMax.ControlType controlType,
+            double Kp,
+            double Ki,
+            double Kd,
+            int kIZone,
+            double tolerance,
+            double fNatural,
+            HYPER_CANSensorSendable sensorToUse,
+            int slotID) {
+        super(name, Kp, Ki, Kd, tolerance);
+        m_pidSlot = slotID;
+        m_motor = motor;
+        m_sensor = sensorToUse;
+        m_pidController = m_motor.getPIDController();
+        m_pidController.setFeedbackDevice(m_sensor.getSensor());
+        m_controlType = controlType;
+        m_IZone = m_prefs.addInt("IZone", kIZone);
+        m_maxVelocityUnits = m_prefs.addDouble("F Natural", fNatural);
 
-    // TODO: determine if we need this or if the PreferencesUpdater can trigger this and have it
-    // grab the data
-    this.onPreferencesUpdated();
-  }
-
-  /**
-   * @param maxVel limit the velocity in RPM of the pid controller
-   * @param minVel lower bound in RPM of the pid controller
-   * @param maxAcc will limit the acceleration in RPM^2
-   * @param accelStrategy acceleration strategy to use for the generated motion profile
-   */
-  public void initSmartMotionProfile(
-      double maxVel,
-      double minVel,
-      double maxAcc,
-      SparkMaxPIDController.AccelStrategy accelStrategy) {
-    if (m_controlType == CANSparkMax.ControlType.kSmartMotion) {
-      if (accelStrategy == SparkMaxPIDController.AccelStrategy.kTrapezoidal) {
-        useSmartMotion = true;
-
-        m_smartMinVel = m_prefs.addDouble("SmartMotion Min Velocity", minVel);
-        m_smartMaxVel = m_prefs.addDouble("SmartMotion Max Velocity", maxVel);
-        m_smartMaxAccel = m_prefs.addDouble("SmartMotion Max Acceleration", maxAcc);
-
-        m_pidController.setSmartMotionAccelStrategy(accelStrategy, m_pidSlot);
-        m_pidController.setSmartMotionMaxVelocity(m_smartMaxVel.get(), m_pidSlot);
-        m_pidController.setSmartMotionMinOutputVelocity(m_smartMinVel.get(), m_pidSlot);
-        m_pidController.setSmartMotionMaxAccel(m_smartMaxAccel.get(), m_pidSlot);
-        m_pidController.setSmartMotionAllowedClosedLoopError(m_tolerance_pref.get(), m_pidSlot);
-      } else {
-        throw new IllegalArgumentException(
-            "As of the 2022 FRC season, SparkMax firmware only supports the trapezoidal motion profiling acceleration strategy");
-      }
-    } else {
-      throw new IllegalStateException(
-          "SparkMaxPID is not using SmartMotion ControlType, cannot set Smart Motion Profile");
+        // TODO: determine if we need this or if the PreferencesUpdater can trigger this and have it
+        // grab the data
+        this.onPreferencesUpdated();
     }
-  }
 
-  /** {@inheritDoc} */
-  public void initSmartMotionProfile(
-      double maxVel, double minVel, double maxAcc) {
-    initSmartMotionProfile(
-        maxVel, minVel, maxAcc, SparkMaxPIDController.AccelStrategy.kTrapezoidal);
-  }
+    /**
+     * @param maxVel        limit the velocity in RPM of the pid controller
+     * @param minVel        lower bound in RPM of the pid controller
+     * @param maxAcc        will limit the acceleration in RPM^2
+     * @param accelStrategy acceleration strategy to use for the generated motion profile
+     */
+    public void initSmartMotionProfile(
+            double maxVel,
+            double minVel,
+            double maxAcc,
+            SparkMaxPIDController.AccelStrategy accelStrategy) {
+        if (m_controlType == CANSparkMax.ControlType.kSmartMotion) {
+            if (accelStrategy == SparkMaxPIDController.AccelStrategy.kTrapezoidal) {
+                useSmartMotion = true;
 
-  public void setSetpoint(double setpoint) {
-    m_setPoint = setpoint;
-  }
+                m_smartMinVel = m_prefs.addDouble("SmartMotion Min Velocity", minVel);
+                m_smartMaxVel = m_prefs.addDouble("SmartMotion Max Velocity", maxVel);
+                m_smartMaxAccel = m_prefs.addDouble("SmartMotion Max Acceleration", maxAcc);
 
-  @Override
-  public double getSpeed() {
-    return m_motor.get();
-  }
-
-  @Override
-  public void execute() {}
-
-  @Override
-  public void enable() {
-    super.enable();
-    m_pidController.setReference(friendlyToNative(m_setPoint), m_controlType, m_pidSlot);
-  }
-
-  @Override
-  public void disable() {
-    super.disable();
-    m_motor.stopMotor();
-  }
-
-  @Override
-  public double getFromSource() {
-    switch (m_controlType) {
-      case kVelocity:
-      case kSmartVelocity:
-        return nativeToFriendly(m_sensor.getVelocity());
-      case kPosition:
-      case kSmartMotion:
-        return nativeToFriendly(m_sensor.getPosition());
-      default:
-        return 0.0;
+                m_pidController.setSmartMotionAccelStrategy(accelStrategy, m_pidSlot);
+                m_pidController.setSmartMotionMaxVelocity(m_smartMaxVel.get(), m_pidSlot);
+                m_pidController.setSmartMotionMinOutputVelocity(m_smartMinVel.get(), m_pidSlot);
+                m_pidController.setSmartMotionMaxAccel(m_smartMaxAccel.get(), m_pidSlot);
+                m_pidController.setSmartMotionAllowedClosedLoopError(m_tolerance_pref.get(), m_pidSlot);
+            } else {
+                throw new IllegalArgumentException(
+                        "As of the 2022 FRC season, SparkMax firmware only supports the trapezoidal motion profiling acceleration strategy");
+            }
+        } else {
+            throw new IllegalStateException(
+                    "SparkMaxPID is not using SmartMotion ControlType, cannot set Smart Motion Profile");
+        }
     }
-  }
 
-  @Override
-  public boolean onTarget() {
-    return onTarget(m_setPoint);
-  }
-
-  @Override
-  public void onPreferencesUpdated() {
-    super.onPreferencesUpdated();
-    m_pidController.setIZone(m_IZone.get(), m_pidSlot);
-    m_pidController.setFF(1 / m_maxVelocityUnits.get(), m_pidSlot);
-
-    if(useSmartMotion) {
-      m_pidController.setSmartMotionMaxVelocity(m_smartMaxVel.get(), m_pidSlot);
-      m_pidController.setSmartMotionMinOutputVelocity(m_smartMinVel.get(), m_pidSlot);
-      m_pidController.setSmartMotionMaxAccel(m_smartMaxAccel.get(), m_pidSlot);
-      m_pidController.setSmartMotionAllowedClosedLoopError(m_tolerance_pref.get(), m_pidSlot);
+    /**
+     * {@inheritDoc}
+     */
+    public void initSmartMotionProfile(
+            double maxVel, double minVel, double maxAcc) {
+        initSmartMotionProfile(
+                maxVel, minVel, maxAcc, SparkMaxPIDController.AccelStrategy.kTrapezoidal);
     }
-  }
 
-  @Override
-  protected void updateOutputRange() {
-    super.updateOutputRange();
-    m_pidController.setOutputRange(m_minOut, m_maxOut, m_pidSlot);
-  }
+    public void setSetpoint(double setpoint) {
+        m_setPoint = setpoint;
+    }
 
-  @Override
-  protected void setPID(double Kp, double Ki, double Kd) {
-    m_pidController.setP(Kp, m_pidSlot);
-    m_pidController.setI(Ki, m_pidSlot);
-    m_pidController.setD(Kd, m_pidSlot);
-  }
+    @Override
+    public double getSpeed() {
+        return m_motor.get();
+    }
 
-  @Deprecated
-  protected void setTolerance(double tolerance) {}
+    @Override
+    public void execute() {
+    }
 
-  @Override
-  public void initSendable(SendableBuilder builder) {
-    super.initSendable(builder);
-    builder.addDoubleProperty("Setpoint", () -> m_setPoint, null);
-  }
+    @Override
+    public void enable() {
+        super.enable();
+        m_pidController.setReference(friendlyToNative(m_setPoint), m_controlType, m_pidSlot);
+    }
 
-  @Override
-  protected void setSmartDashboardType(SendableBuilder builder) {
-    builder.setSmartDashboardType("SparkMax PID");
-  }
+    @Override
+    public void disable() {
+        super.disable();
+        m_motor.stopMotor();
+    }
 
-  @Override
-  public void disableContinuousInput() {
-    m_pidController.setPositionPIDWrappingEnabled(false);
-  }
+    @Override
+    public double getFromSource() {
+        switch (m_controlType) {
+            case kVelocity:
+            case kSmartVelocity:
+                return nativeToFriendly(m_sensor.getVelocity());
+            case kPosition:
+            case kSmartMotion:
+                return nativeToFriendly(m_sensor.getPosition());
+            default:
+                return 0.0;
+        }
+    }
 
-  @Override
-  public void enableContinuousInput(double minIn, double maxIn) {
-    m_pidController.setPositionPIDWrappingEnabled(true);
-    m_pidController.setPositionPIDWrappingMinInput(friendlyToNative(minIn));
-    m_pidController.setPositionPIDWrappingMaxInput(friendlyToNative(maxIn));
-  }
+    @Override
+    public boolean onTarget() {
+        return onTarget(m_setPoint);
+    }
+
+    @Override
+    public void onPreferencesUpdated() {
+        super.onPreferencesUpdated();
+        m_pidController.setIZone(m_IZone.get(), m_pidSlot);
+        double maxVelocityUnits = m_maxVelocityUnits.get();
+        if(maxVelocityUnits != 0) {
+          m_pidController.setFF(1 / maxVelocityUnits, m_pidSlot);
+        }
+
+        if (useSmartMotion) {
+            m_pidController.setSmartMotionMaxVelocity(m_smartMaxVel.get(), m_pidSlot);
+            m_pidController.setSmartMotionMinOutputVelocity(m_smartMinVel.get(), m_pidSlot);
+            m_pidController.setSmartMotionMaxAccel(m_smartMaxAccel.get(), m_pidSlot);
+            m_pidController.setSmartMotionAllowedClosedLoopError(m_tolerance_pref.get(), m_pidSlot);
+        }
+    }
+
+    @Override
+    protected void updateOutputRange() {
+        super.updateOutputRange();
+        m_pidController.setOutputRange(m_minOut, m_maxOut, m_pidSlot);
+    }
+
+    @Override
+    protected void setPID(double Kp, double Ki, double Kd) {
+        m_pidController.setP(Kp, m_pidSlot);
+        m_pidController.setI(Ki, m_pidSlot);
+        m_pidController.setD(Kd, m_pidSlot);
+    }
+
+    @Deprecated
+    protected void setTolerance(double tolerance) {
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        super.initSendable(builder);
+        builder.addDoubleProperty("Setpoint", () -> m_setPoint, null);
+    }
+
+    @Override
+    protected void setSmartDashboardType(SendableBuilder builder) {
+        builder.setSmartDashboardType("SparkMax PID");
+    }
+
+    @Override
+    public void disableContinuousInput() {
+        m_pidController.setPositionPIDWrappingEnabled(false);
+    }
+
+    @Override
+    public void enableContinuousInput(double minIn, double maxIn) {
+        m_pidController.setPositionPIDWrappingEnabled(true);
+        m_pidController.setPositionPIDWrappingMinInput(friendlyToNative(minIn));
+        m_pidController.setPositionPIDWrappingMaxInput(friendlyToNative(maxIn));
+    }
 }
